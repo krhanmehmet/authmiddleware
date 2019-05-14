132f9864f3df21f64007a590c911b94fc4afa9d5
mehmet <mehmet.kurhan@parabolyazilim.com>
2019-05-14 16:00:42 +0300

    package json is being added

---
+++ index.ts
@@ -0,0 +1,53 @@
+
+import * as express  from 'express';
+import axios from 'axios';
+
+
+
+
+function hasAccess(user : any , config : any) {
+    if(user.principal && user.principal.roles && user.principal.roles.bluesense) {
+        if(user.principal.roles.bluesense.permission >= config.role) {
+            return true;
+        }
+    }
+    if(config.scope) {
+        if(user.details.clientDetails.scope) {
+            for(var i = 0; i < user.details.clientDetails.scope.length; i++) {
+                if(config.scope === user.details.clientDetails.scope[i]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function AuthMiddleware() {
+    return (config: { scope? : string, role? : number }) => {
+        return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            if(!req.headers.authorization) {
+                res.sendStatus(401);
+            }
+            (async () => {
+                axios.get("http://auth-server:8765/uaa/users/current",{
+                    headers : {
+                        authorization : req.headers.authorization
+                    }
+                }).then(data => {
+                    if(hasAccess(data.data,config)) {
+                        next();
+                    } else {
+                        res.sendStatus(401);
+                    }
+                }).catch(err=> {
+                    res.sendStatus(401);
+                })
+            })();
+        };
+    };
+}
+
+const Auth = AuthMiddleware();
+
+export { Auth };
